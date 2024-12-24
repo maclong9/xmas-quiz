efddccee4a42918dab5b1f20a19355e164e9792f
Mac <maclong9@icloud.com>
2024-12-24 00:34:01 +0000

    fix: merge conflicts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,6 @@
 	class="mx-auto flex max-w-2xl flex-col items-center justify-center px-4 py-8"
 	in:fade={{ duration: 300 }}
 >
-<<<<<<< HEAD
-  <div
-    class="text-center space-y-6"
-    in:fly={{ y: 20, duration: 500, delay: 200 }}
-  >
-    <h1 class="text-4xl font-bold mb-4">Yana's Version!</h1>
-
-    <p class="text-lg text-gray-600 leading-relaxed max-w-xl mx-auto">
-      Filled with questions related to cats, Sabrina Carpenter, witches and Belarus, Enjoy!
-    </p>
-=======
 	<div class="space-y-6 text-center" in:fly={{ y: 20, duration: 500, delay: 200 }}>
 		<h1 class="mb-4 text-4xl font-bold">Welcome to the Xmas Quiz!</h1>
 
@@ -25,7 +14,6 @@
 			cards, I have created a quiz app that has a great selection of questions for everyone to
 			enjoy!
 		</p>
->>>>>>> 9cdb566ad5b4c5a348638d3be144a18b44c802fc
 
 		<a href="/game" class="button" in:scale={{ duration: 400, delay: 500, start: 0.8 }}>
 			Start Game
